0aee8cb3b609831089438eb1ee0358659453f9e1
pazthiago1 <pazthiagolima@gmail.com>
2019-06-12 18:36:53 -0300

    Adiciona condicao para caso o arquivo já existir, faz o delete e grava novamente

--- microservice/src/main/java/com/youthquake/microservice/microservice/controller/TargetMicroservice.java
+++ microservice/src/main/java/com/youthquake/microservice/microservice/controller/TargetMicroservice.java
@@ -31,19 +31,24 @@ public class TargetMicroservice {
 
 	public TargetMicroservice() {
 		rest = new RestTemplate();
-		//url = "http://localhost:8080/target/microservice";
+		// url = "http://localhost:8080/target/microservice";
 		url = "https://serviceyouthquake.azurewebsites.net/target/microservice";
 	}
-	
-	@RequestMapping(path="/downloadcsv", method = RequestMethod.GET)
+
+	@RequestMapping(path = "/downloadcsv", method = RequestMethod.GET)
 	public ResponseEntity<ByteArrayResource> getTargetUserToCSV() throws IOException {
 		Target[] t = this.rest.getForObject(url, Target[].class);
-		
+
 		File file = new File("C:\\Users\\Aluno\\Desktop\\Youthquake-targets.csv");
-		
+
+		// deleta arquivo após ele ser baixado para evitar repetições xD
+		if (file.exists()) {
+			file.delete();
+		}
+
 		FileWriter writer = new FileWriter(file, true);
 		CSVWriter csv = new CSVWriter(writer, ';', CSVWriter.NO_QUOTE_CHARACTER, CSVWriter.DEFAULT_ESCAPE_CHARACTER,
-		CSVWriter.DEFAULT_LINE_END);
+				CSVWriter.DEFAULT_LINE_END);
 
 		csv.writeNext(new String[] { "ID_target", "Description", "Date_Start", "Date_End", "Name", "Percentage",
 				"Value", "Value_Accumulated" });
@@ -53,22 +58,19 @@ public class TargetMicroservice {
 					targets.getDtEnd(), targets.getName(), targets.getPercentage(), targets.getValue(),
 					targets.getValueAccumulated() });
 		}
-		
+
 		HttpHeaders header = new HttpHeaders();
 		header.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=Youthquake-targets.csv");
 		header.add("Cache-Control", "no-cache, no-store, must-revalidate");
-        header.add("Pragma", "no-cache");
-        header.add("Expires", "0");
-		
-        csv.close();
-        
-        Path path = Paths.get(file.getAbsolutePath());
-        ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(path));
-        
-        return ResponseEntity.ok()
-        		.headers(header)
-        		.contentLength(file.length())
-        		.contentType(MediaType.parseMediaType("application/octet-stream"))
-        		.body(resource);
+		header.add("Pragma", "no-cache");
+		header.add("Expires", "0");
+
+		csv.close();
+
+		Path path = Paths.get(file.getAbsolutePath());
+		ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(path));
+
+		return ResponseEntity.ok().headers(header).contentLength(file.length())
+				.contentType(MediaType.parseMediaType("application/octet-stream")).body(resource);
 	}
 }
